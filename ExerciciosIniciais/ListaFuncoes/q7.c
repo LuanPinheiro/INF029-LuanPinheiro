e4df0dd170b3726cf353cb3d6cda78d7954c162b
Luan <57464923+LuanPinheiro@users.noreply.github.com>
2022-09-19 15:26:03 +0000

    As palavras agora estão dentro de uma struct, ao invés de serem 3 vetores isolados

--- ExerciciosIniciais/ListaFuncoes/q7.c
+++ ExerciciosIniciais/ListaFuncoes/q7.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
 
-void ler3palavras(char p1[16], char p2[16], char p3[16]);
+typedef struct{
+  char palavra1[16];
+  char palavra2[16];
+  char palavra3[16];
+}words;
+
+words ler3palavras(words palavras);
 void LimparString(char string[]);
 
 int main() {
 
-  char palavra1[16];
-  char palavra2[16];
-  char palavra3[16];
+  words palavras;
 
-  ler3palavras(palavra1, palavra2, palavra3);
+  palavras = ler3palavras(palavras);
 
-  printf("\n%s\n", palavra1);
-  printf("%s\n", palavra2);
-  printf("%s\n", palavra3);
+  printf("\n%s\n", palavras.palavra1);
+  printf("%s\n", palavras.palavra2);
+  printf("%s\n", palavras.palavra3);
   
   return 0;
 }
 
-void ler3palavras(char p1[16], char p2[16], char p3[16]){
+words ler3palavras(words palavras){
 
   printf("Palavra 1: ");
-  fgets(p1, 16, stdin);
-  LimparString(p1);
+  fgets(palavras.palavra1, 16, stdin);
+  LimparString(palavras.palavra1);
 
   printf("Palavra 2: ");
-  fgets(p2, 16, stdin);
-  LimparString(p2);
+  fgets(palavras.palavra2, 16, stdin);
+  LimparString(palavras.palavra2);
   
   printf("Palavra 3: ");
-  fgets(p3, 16, stdin);
-  LimparString(p3);
+  fgets(palavras.palavra3, 16, stdin);
+  LimparString(palavras.palavra1);
+
+  return palavras;
 }
 
 void LimparString(char string[]){
